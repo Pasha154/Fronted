540e29f4db7d7bda19d03f5db3b509100dce3a2c
kolunpavlo <pavel154k@gmail.com>
2024-09-06 10:57:15 +0300

    Commit l3h2

---
+++ main.js
@@ -0,0 +1,5 @@
+let firstString = prompt("First name:");
+let secondString = prompt("Second name:");
+let thirdString = prompt("Third name");
+
+alert(`${firstString},  ${secondString},  ${thirdString}`);
