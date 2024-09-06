6e208534d884cba41ac7952451e269ac5e8191d4
kolunpavlo <pavel154k@gmail.com>
2024-09-06 10:32:57 +0300

    Commit l3h1

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("String");
+console.log("Number");
+console.log("Null");
+console.log("Undefined");
+console.log("Symbol");
+console.log("Object");
+console.log("BigInt");
+console.log("Boolean");
